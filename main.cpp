560845c9309f92131e373665f45cf098b4417861
SneerCherryson <patryk_pogoda@vp.pl>
2017-06-18 19:02:02 +0200

    Second commit. Implementation of negation in L3. It is so ugly rigth now.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <QCoreApplication>
+#include <iostream>
+#include "lukasiewiczlogic.hpp"
+using namespace std;
 
-int main(int argc, char *argv[])
+int main ()
 {
-    QCoreApplication a(argc, argv);
 
-    return a.exec();
+
+    int valueToNegate;
+    for(;;)
+    {
+        cout << "Which value should be negate: 0) false, 1) true, 2) possibility, 3) end program: " << endl;
+        cin >> valueToNegate;
+        if(3 == valueToNegate) {
+            cout<<"See you later!"<<endl;
+            return 0;
+        }
+        auto l_value = convertIntToEnum(valueToNegate);
+        cout<< "Negation is: " << toString(!l_value) << endl;
+
+    }
+
+    return 0;
 }
