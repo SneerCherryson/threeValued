98b8d27eaefab01474a95daad399c31db9d95759
SneerCherryson <patryk_pogoda@vp.pl>
2018-03-24 00:28:06 +0100

    Small update to small refactoring.

--- commonMethods.hpp
+++ commonMethods.hpp
@@ -38,20 +38,21 @@ void displayMenu()
         cout << "Which value should be negate: 0) false, 1) true, 2) possibility, 3) end program: " << endl;
         cin>>valueToNegate;
 
-        if(cin.fail())
+        if(cin.good())
         {
-            cout<<"It is wrong type value, please insert proper one."<<endl;
-            break;
+            if(3 == valueToNegate) {
+                cout<<"See you later!"<<endl;
+                return;
+            }
+            auto l_value = convertIntToEnum(valueToNegate);
+            cout<< "Choosen value: " << toString(l_value) << endl;
+            cout<< "Negation is: " << toString(!l_value) << endl;
+            cout<< "Necessity opertator product result: " << toString(&l_value) << endl;
+            cout<< "Possibility opertator product result: " << toString(~l_value) << endl<<endl;
+            continue;
         }
-        if(3 == valueToNegate) {
-            cout<<"See you later!"<<endl;
-            return;
-        }
-        auto l_value = convertIntToEnum(valueToNegate);
-        cout<< "Choosen value: " << toString(l_value) << endl;
-        cout<< "Negation is: " << toString(!l_value) << endl;
-        cout<< "Necessity opertator product result: " << toString(&l_value) << endl;
-        cout<< "Possibility opertator product result: " << toString(~l_value) << endl<<endl;
+        cout<<"It is wrong type value, please insert proper one."<<endl;
+        break;
     }
 }
 
